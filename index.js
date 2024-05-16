daeb2ee3e926260370b16ad3c8c1f0392c52ed42
akwhyt <ha05121977@gmail.com>
2024-05-16 20:11:11 +0900

    First commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.post("/api/v1/quiz", function (req, res) {
+  const answer = req.body.answer;
+  if (answer == 2) {
+    res.redirect("/correct.html");
+    // res.send("正解");
+  } else {
+    res.redirect("/wrong.html");
+    // res.send("不正解");
+  }
+});
+
+app.get("/api/v1/users", function (req, res) {
+  res.send({
+    name: "Mike",
+    age: 30,
+  });
+});
+
+app.listen(3000, function () {
+  console.log("I am running");
+});
+
+console.log("最終行");
